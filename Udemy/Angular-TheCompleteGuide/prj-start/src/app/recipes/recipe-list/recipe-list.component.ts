cebaa3969e7bfff9ed5938c588657aee3aa244bd
Jay Benedicto <soamazing@gmail.com>
2020-12-29 07:09:06 -0300

    +

--- Udemy/Angular-TheCompleteGuide/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Udemy/Angular-TheCompleteGuide/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test",
+      "Desc test",
+      "https://cleanfoodcrush.com/wp-content/uploads/2018/08/Clean-Sweet-Paprika-Chicken-Cauli-22Rice22-Bowls-Recipes-1024x683.jpg"
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

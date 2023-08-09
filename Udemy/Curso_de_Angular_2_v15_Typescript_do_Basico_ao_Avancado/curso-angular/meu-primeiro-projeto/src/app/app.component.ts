559d5553344b44cb27f8668e751f4b8e7a85ee66
Jaqueline Benedicto <soamazing@gmail.com>
2023-08-09 19:38:04 -0300

    evolução do curso de angular da Udemy

--- Udemy/Curso_de_Angular_2_v15_Typescript_do_Basico_ao_Avancado/curso-angular/meu-primeiro-projeto/src/app/app.component.ts
+++ Udemy/Curso_de_Angular_2_v15_Typescript_do_Basico_ao_Avancado/curso-angular/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <app-title
+      [className]="classNameTitle"
+      title="Bem-vindo(a)"
+    ></app-title>
+    <app-data-binding></app-data-binding>
+    <router-outlet></router-outlet>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meu-primeiro-projeto';
+
+  classNameTitle: string = 'my_title';
+
+  ngOnInit(): void {
+    console.log(this.initConsole);
+  }
+
+  initConsole: string = 'Iniciou o AppComponent!!';
 }
--- Udemy/Curso_de_Angular_2_v15_Typescript_do_Basico_ao_Avancado/curso-angular/meu-primeiro-projeto/src/app/app.module.ts
+++ Udemy/Curso_de_Angular_2_v15_Typescript_do_Basico_ao_Avancado/curso-angular/meu-primeiro-projeto/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,

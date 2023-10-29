311db1b0d92420f0521f8432895b981541c30351
Jaqueline Benedicto <soamazing@gmail.com>
2023-10-29 08:23:25 -0300

    angular

---
+++ Udemy/Angular_with_Test_Driven_Development/source/src/app/app.component.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    })
+  );
+
+  it('passes', () => {
+    expect(true).toBe(true);
+  });
+});
